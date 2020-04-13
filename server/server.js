462b33457af37c845aae99a8fd0181c19f805549
JoseLuis <joseluisdelacruzgarrido@gmail.com>
2020-04-13 19:49:19 +0200

    First commit

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require('./config/config');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+    // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuario', function(req, res) {
+    let persona = req.body;
+
+    if (persona.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        })
+    } else {
+        res.json({
+            persona
+        })
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json({
+        id
+    })
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Excuchando puerto 3000");
+})
